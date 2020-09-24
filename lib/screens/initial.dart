1fdd44cac4e62a9843a35e45472bc1c38e78f5d6
AlexHBall <timmygaines22@gmail.com>
2020-09-24 18:43:47 +0200

    refactor initial

--- lib/screens/initial.dart
+++ lib/screens/initial.dart
@@ -5,28 +5,36 @@ import 'package:inditask/widgets/widgets.dart';
 class InitialScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return TaskWidget();
+    return AddTaskWidget();
   }
 }
 
-class TaskWidget extends State<InitialScreen> {
+class AddTaskWidget extends State<InitialScreen> {
+  Widget image() {
+    return Padding(
+      padding: EdgeInsets.only(top: 142.0),
+      child: Image.asset('assets/images/logoSmall.png'),
+    );
+  }
+
+  Widget textHeader() {
+    return Padding(
+      padding:
+          EdgeInsets.only(top: 28.0, left: 55.0, right: 50.0, bottom: 20.0),
+      child: Text("Create your first task",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colour.blue.color)),
+    );
+  }
+
   Column initialPage() {
     return Column(
       children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 142.0),
-          child: Image.asset('assets/images/logoSmall.png'),
-        ),
-        Padding(
-          padding:
-              EdgeInsets.only(top: 28.0, left: 55.0, right: 50.0, bottom: 20.0),
-          child: Text("Create your first task",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colour.blue.color)),
-        ),
+        image(),
+        textHeader(),
         AddTask(
           costToggled: false,
           isModal: false,
