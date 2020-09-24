df9dd99ad8bd43d2e8ce7c69021d82b4c3c115a9
AlexHBall <timmygaines22@gmail.com>
2020-09-24 19:15:14 +0200

    refactor taskcards

--- lib/screens/dashboard/taskcard.dart
+++ lib/screens/dashboard/taskcard.dart
@@ -1,6 +1,9 @@
 part of 'dashboard.dart';
 
-class EditButton extends StatelessWidget {
+class ButtonWrapper extends StatelessWidget {
+  final Widget child;
+  const ButtonWrapper({this.child});
+
   @override
   Widget build(BuildContext context) {
     return ButtonTheme(
@@ -14,10 +17,19 @@ class EditButton extends StatelessWidget {
         textColor: Colors.white,
         padding: EdgeInsets.all(8.0),
         onPressed: () {},
-        child: Text(
-          "Edit",
-          style: TextStyle(fontSize: 8.0, fontWeight: FontWeight.w600),
-        ),
+        child: child,
+      ),
+    );
+  }
+}
+
+class EditButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ButtonWrapper(
+      child: Text(
+        "Edit",
+        style: TextStyle(fontSize: 8.0, fontWeight: FontWeight.w600),
       ),
     );
   }
@@ -26,20 +38,7 @@ class EditButton extends StatelessWidget {
 class AlarmButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ButtonTheme(
-      minWidth: 41.35,
-      height: 27.57,
-      child: FlatButton(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-            side: BorderSide(color: Colors.white)),
-        // color: Colors.white,
-        textColor: Colors.white,
-        padding: EdgeInsets.all(8.0),
-        onPressed: () {},
-        child: Icon(Icons.add_alarm, size: 12),
-      ),
-    );
+    return ButtonWrapper(child: Icon(Icons.add_alarm, size: 12));
   }
 }
 
@@ -55,30 +54,64 @@ class TaskCard extends StatelessWidget {
       @required this.backgroundColor})
       : super(key: key);
 
+  Widget score() {
+    return Container(
+      height: 350,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: backgroundColor,
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          cost.toString(),
+          style: TextStyle(
+              color: Colors.white, fontSize: 149, fontWeight: FontWeight.w600),
+          textAlign: TextAlign.center,
+        ),
+      ), // 1C263
+    );
+  }
+
+  Widget cardButtons() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 17.0, top: 12.0),
+        child: Row(
+          children: [
+            EditButton(),
+            SizedBox(
+              width: 3.0,
+            ),
+            AlarmButton(),
+          ],
+        ));
+  }
+
+  Widget descriptionText() {
+    return Padding(
+      padding: const EdgeInsets.only(
+          left: 53.0, top: 90.0, right: 30.0, bottom: 50.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          description,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 3,
+          style: TextStyle(
+              color: Colors.white, fontSize: 23, fontWeight: FontWeight.w600),
+          textAlign: TextAlign.left,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(
           left: 40.0, right: 40.0, top: 20.0, bottom: 38.0),
       child: Stack(children: [
-        Container(
-          height: 350,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: backgroundColor,
-          ),
-          child: Align(
-            alignment: Alignment.center,
-            child: Text(
-              cost.toString(),
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 149,
-                  fontWeight: FontWeight.w600),
-              textAlign: TextAlign.center,
-            ),
-          ), // 1C263
-        ),
+        score(),
         Container(
           height: 350,
           decoration: BoxDecoration(
@@ -86,35 +119,8 @@ class TaskCard extends StatelessWidget {
             color: backgroundColor.withOpacity(0.5),
           ),
           child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 17.0, top: 12.0),
-              child: Row(
-                children: [
-                  EditButton(),
-                  SizedBox(
-                    width: 3.0,
-                  ),
-                  AlarmButton(),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 53.0, top: 90.0, right: 30.0, bottom: 50.0),
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  description,
-                  overflow: TextOverflow.ellipsis,  maxLines: 3,
-
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 23,
-                      fontWeight: FontWeight.w600),
-                  textAlign: TextAlign.left,
-                ),
-              ),
-            )
+            cardButtons(),
+            descriptionText(),
           ]),
         )
       ]),
